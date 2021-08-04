6e5c9818d5fe95fe3d300ead6fe12216292e2dcb
Anthoni Mathias <anthonimathias@gmail.com>
2021-08-04 17:12:24 +0100

    Add game route to run.py, and create game.html to template folder

--- run.py
+++ run.py
@@ -4,18 +4,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 """index"""
 @app.route("/")
 def index():
   return render_template("index.html")
 
+
 @app.route("/about")
 def about():
   return render_template("about.html")  
 
+
 @app.route("/contact")
 def contact():
   return render_template("contact.html")  
+
+
+@app.route("/game")
+def game():
+  return render_template("game.html")
+
 
 if __name__ == "__main__":
   app.run(
